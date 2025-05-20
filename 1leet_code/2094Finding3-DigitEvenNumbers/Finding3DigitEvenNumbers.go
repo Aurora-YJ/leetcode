0c538bf163e4e7621b4908a6a7568541107632c8
Yossf-jaouhar <jwhryosf@gmail.com>
2025-05-20 13:29:26 +0100

    findEvenNumbers

---
+++ 1leet_code/2094Finding3-DigitEvenNumbers/Finding3DigitEvenNumbers.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	digits := []int{2,2,8,8,2}
+	res := findEvenNumbers(digits)
+	fmt.Println("---->",res)
+}
+
+func findEvenNumbers(digits []int) []int {
+	var res []int
+	//mapp := make(map[int]bool)
+
+	for i := range digits {
+		for j := range digits {
+			for k := range digits {
+				if i != j && j != k  && k != i {
+					r := digits[i] * 100 + digits[j] * 10 + digits[k]
+					if r > 100 && r < 1000 && r % 2 == 0 {
+						res = append(res, r)
+					}
+				}
+			}			
+
+		}
+	}
+
+	for i := 0; i < len(res); i++ {
+		for j := 0; j < len(res)-1-i; j++ {
+			if  res[j] > res[j+1] {
+				res[j] , res[j+1] = res[j+1] , res[j]
+			}
+		}
+	}
+
+	return res
+}
