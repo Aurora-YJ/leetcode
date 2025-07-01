14a08cb5b962fda3a8bf243b964b62147a66487d
Yossf-jaouhar <jwhryosf@gmail.com>
2025-07-02 00:26:35 +0100

    is hard

--- 2leet_code/2099.Find_Subsequence_of_Length_KWiththe_Largest_Sum/2099.Find_Subsequence_of_Length_KWiththe_Largest_Sum.go
+++ 2leet_code/2099.Find_Subsequence_of_Length_KWiththe_Largest_Sum/2099.Find_Subsequence_of_Length_KWiththe_Largest_Sum.go
@@ -1,9 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	nums := []int{-1,-2,3,4}
+	nums := []int{-1, -2, 3, 4}
 	k := 3
 
 	result := maxSubsequence(nums, k)
@@ -17,35 +19,69 @@ func maxSubsequence(nums []int, k int) []int {
 		return []int{}
 	}
 
-	lastres := 0
-
-	ll := []int{}
+	mapp := make(map[int][]int)
 
 	for i := 0; i < len(nums); i++ {
-		res := 0
-		res += nums[i]
-		v := 0
+		mapp[nums[i]] = append(mapp[nums[i]], nums[i])
+
+		res := []int{}
 		for j := 0; j < len(nums); j++ {
-			if i == j {
+			res = append(res, nums[j])
+		}
+		newRes := checker(res, k-1, nums[i])
+		mapp[nums[i]] = append(mapp[nums[i]], newRes...)
+
+	}
+
+	hh := 0
+	jj := 0
+	result := []int{}
+	for _,v := range mapp {
+
+		for _,h := range v {
+			hh += h
+		}
+
+
+		if hh > jj {
+			jj = hh
+			result = v
+		}
+		hh = 0
+
+	}
+
+	fmt.Println("jj", jj)
+
+	return result
+
+}
+
+func checker(res []int, k int, intger int) []int {
+	result := []int{}
+	for k > 0 {
+		helper := 0
+		index := 0
+
+		for i := 0; i < len(res); i++ {
+			if res[i] == intger {
+				continue
+			}
+			if helper == 0 {
+				helper = res[i]
+				index = i
 				continue
 			}
 
-			v++
-			if v  <k {
-				res += nums[j]
-			} else {
-				break
+			if helper < res[i] {
+				helper = res[i]
+				index = i
 			}
-			
-			fmt.Println("jjj", res)
-		}
-		fmt.Println("-------")
-		if res > lastres {
-			lastres = res
-			res = 0
-		} 
-	}
-	fmt.Println("kk", lastres)
-	return ll
 
+		}
+		res = append(res[:index], res[index+1:]...)
+		result = append(result, helper)
+		k--
+	}
+	return result
 }
