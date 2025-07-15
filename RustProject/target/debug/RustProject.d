7838dcd5200f17690d4a9aed739fa13da6687455
Yossf-jaouhar <jwhryosf@gmail.com>
2025-07-15 14:53:55 +0100

    Add .gitignore to ignore target and Cargo.lock

--- RustProject/target/debug/RustProject.d
+++
@@ -1,1 +0,0 @@
-/home/jaouhar/Desktop/leetcode/RustProject/target/debug/RustProject: /home/jaouhar/Desktop/leetcode/RustProject/src/main.rs /home/jaouhar/Desktop/leetcode/RustProject/src/roman_to_integer.rs
