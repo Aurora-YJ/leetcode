4026375c62fb6573dad446ab3dc8dace71a12b12
Yossf-jaouhar <jwhryosf@gmail.com>
2025-07-15 21:25:11 +0100

    add binary

--- 2leet_code/2099.Find_Subsequence_of_Length_KWiththe_Largest_Sum/2099.Find_Subsequence_of_Length_KWiththe_Largest_Sum.go
+++ 2leet_code/2099.Find_Subsequence_of_Length_KWiththe_Largest_Sum/2099.Find_Subsequence_of_Length_KWiththe_Largest_Sum.go
@@ -1,9 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	nums := []int{2,1,3,3}
+	nums := []int{7,3,9,6}
 	k := 2
 
 	result := maxSubsequence(nums, k)
@@ -18,43 +20,5 @@ func maxSubsequence(nums []int, k int) []int {
 
 	res := []int{}
 
-	mapp := make(map[int]int)
-
-	res = append(res, nums...)
-
-	for k > 0 {
-		index := checker(res)
-		mapp[res[index]]++
-
-		res = append(res[:index], res[index+1:]...)
-		k--
-	}
-	result := []int{}
-	for h := range mapp {
-
-		for i := 0; i < len(nums); i++ {
-			if h == nums[i] {
-				result = append(result, h)
-			}
-			if mapp[h] >= 1 {
-				mapp[h]--
-			} else {
-				break
-			}
-		}
-	}
-
-	return result
-}
-
-func checker(res []int) int {
-	help := res[0]
-	in := 0
-	for i := 1; i < len(res); i++ {
-		if help < res[i] {
-			help = res[i]
-			in = i
-		}
-	}
-	return in
+	return res
 }
