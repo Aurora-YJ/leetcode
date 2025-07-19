6ef9ab046d6acf14c64815768fcea93a057f51f8
Yossf-jaouhar <jwhryosf@gmail.com>
2025-07-19 01:08:10 +0100

    fix the maxSubsequence

--- 2leet_code/2099.Find_Subsequence_of_Length_KWiththe_Largest_Sum/2099.Find_Subsequence_of_Length_KWiththe_Largest_Sum.go
+++ 2leet_code/2099.Find_Subsequence_of_Length_KWiththe_Largest_Sum/2099.Find_Subsequence_of_Length_KWiththe_Largest_Sum.go
@@ -5,19 +5,25 @@ import (
 )
 
 func main() {
-	nums := []int{-56, -214, -345, 952, 529, -294, -325, -924, -27, -41, 716, -67, -65, -481}
-	k := 5
+	// nums := []int{-56, -214, -345, 952, 529, -294, -325, -924, -27, -41, 716, -67, -65, -481}
+	nums := []int{
+		-16, -13, 8, 16, 35, -17, 30, -8, 34, -2, -29, -35, 15, 13, -30, -34, 6, 15, 28, -23, 34, 28, -24, 15, -17, 10, 31, 32, -3,
+		-36, 19, 31, -5, -21, -33, -18, -23, -37, -15, 12, -28, -40, 1, 38, 38, -38, 33, -35, -28, -40, 4, -15, -29, -33, -18, -9, -29, 20, 1,
+		36, -8, 23, -34, 16, -7, 13, 39, 38, 7, -7, -10, 30, 9, 26, 27, -37, -18, -25, 14, -36, 23, 28, -15, 35, -9, 1,
+	}
 
+	// nums := []int{2, 5, 3, 5}
+	k := 8
 	/*
 
-		[952,716,-67,-65,-481]
+				Output
+		[35,34,34,38,38,36,39,38]
 		Expected
-		[952,529,-27,-41,716]
+		[35,34,38,38,36,39,38,35]
 
 	*/
 
 	result := maxSubsequence(nums, k)
-
 	fmt.Println("Max subsequence of length", k, ":", result)
 }
 
@@ -25,17 +31,38 @@ func maxSubsequence(nums []int, k int) []int {
 	if k > len(nums) {
 		return []int{}
 	}
-
+	n := len(nums)
 	res := []int{}
 
-	toRemove := len(nums) - k
-	for i := 0; i < len(nums); i++ {
-		for len(res) > 0 && res[len(res)-1] < nums[i] && toRemove > 0 && len(nums[i:]) > toRemove {
-			res = res[:len(res)-1]
-			toRemove--
+	res = append(res, nums...)
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			if nums[i] > nums[j] {
+				nums[i], nums[j] = nums[j], nums[i]
+			}
 		}
-		res = append(res, nums[i])
 	}
 
-	return res[:k]
+	mapp := make(map[int]int)
+
+	for _, v := range nums[:k] {
+		mapp[v]++
+	}
+	result := []int{}
+
+	for _, v := range res {
+		for i := 0; i < len(nums[:k]); i++ {
+			if mapp[v] < 1 {
+				break
+			} 
+			if nums[:k][i] == v {
+				result = append(result, v)
+				mapp[v]--
+				break
+			}
+		}
+	}
+
+	return result
 }
