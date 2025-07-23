356932a7171c233f9451d4052460179601549f5c
Yossf-jaouhar <jwhryosf@gmail.com>
2025-07-23 23:13:10 +0100

    fixing the three_sum

--- rust_project/src/main.rs
+++ rust_project/src/main.rs
@@ -1,24 +1,13 @@
-// mod sum_triplet;
+mod sum_triplet;
 
-// use sum_triplet::{Solution};
+use sum_triplet::{Solution};
 
-// fn main() {
-//     let nums = vec![-1,0,1,2,-1,-4];
+fn main() {
+    let nums = vec![-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0];
 
 
 
-//     let result = Solution::three_sum(nums);
+    let result = Solution::three_sum(nums);
 
-//     println!("{:?}", result);
-// }
-
-mod maximum_score_from_removing_substrings;
-
-use maximum_score_from_removing_substrings::{Solution};
-
-fn main(){
-    let (x , y) = (4 , 5);
-
-    let result = Solution::maximum_gain("cdbcbbaaabab".to_string(), x, y);
     println!("{:?}", result);
 }
--- rust_project/src/sum_triplet.rs
+++ rust_project/src/sum_triplet.rs
@@ -2,22 +2,53 @@ pub struct Solution;
 
 impl Solution {
     pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        
-        let mut res: Vec<i32> = Vec::new();
         let mut result: Vec<Vec<i32>> = Vec::new();
 
-        let mut cont = 0;
+        for i in 0..nums.len() {
+            for j in 0..nums.len() {
+                for k in 0..nums.len() {
+                    if i != j && j != k && i != k {
+                        if nums[i] + nums[j] + nums[k] == 0 {
 
-        for i in &nums {
-            cont += i;
-            res.push(*i);
-            if cont == 0 {
-                println!("{}", cont);
-                break;
+                            if Self::is_checked(result.clone(),  nums[i], nums[j], nums[k]) {
+                                result.push(vec![nums[i], nums[j], nums[k]]);
+                            }
+                        } 
+                    }
+                }
             }
         }
 
-        result.push(res);
+
         result
     }
+
+
+   fn is_checked(res: Vec<Vec<i32>>, i: i32, j: i32, k: i32) -> bool {
+        if res.len() == 0 {
+            return true;
+        }
+
+        let mut new_arr = vec![i, j, k];
+
+        new_arr.sort();
+
+        for arr in res {
+
+            let mut sort_arr = arr.clone();
+
+            sort_arr.sort();
+
+            if sort_arr == new_arr {
+                return false;
+            }
+
+        }
+
+        true
+    }
+
+
+
+
 }
