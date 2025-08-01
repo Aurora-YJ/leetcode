e5c7e82d74278134f0c376cb89a4007ce32a6cad
Yossf-jaouhar <jwhryosf@gmail.com>
2025-08-01 07:46:10 +0100

    add generate

--- rust_project/src/main.rs
+++ rust_project/src/main.rs
@@ -8,3 +8,20 @@ fn main() {
 
     println!("{:?}", result);
 }
+
+
+/*
+pub fn generate(num_rows: i32) -> Vec<Vec<i32>> {
+   let mut result: Vec<Vec<i32>> = Vec::new();
+   let mut res: Vec<i32> = Vec::new(); 
+   for i in 1..=num_rows {
+      for j in 0..i {
+         res.push(num_rows);
+      }
+      result.push(res.clone());
+      res.clear();
+   }
+
+   result
+}
+*/
