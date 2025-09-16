32a7b88b8ef717bb5eb10910dd892ae109bd19ef
Yossf-jaouhar <jwhryosf@gmail.com>
2025-09-16 16:20:37 +0100

    start java

--- rust_project/src/main.rs
+++ rust_project/src/main.rs
@@ -1,27 +1,10 @@
-mod count_hills_and_valleys_in_an_array;
+// mod ff;
+// use ff::*;
 
-use count_hills_and_valleys_in_an_array::{Solution};
-
-fn main() {
-    let nums = vec![2,4,1,1,6,5]
-    let result = Solution::count_hill_valley(nums);
-
-    println!("{:?}", result);
-}
-
-
-/*
-pub fn generate(num_rows: i32) -> Vec<Vec<i32>> {
-   let mut result: Vec<Vec<i32>> = Vec::new();
-   let mut res: Vec<i32> = Vec::new(); 
-   for i in 1..=num_rows {
-      for j in 0..i {
-         res.push(num_rows);
-      }
-      result.push(res.clone());
-      res.clear();
-   }
-
-   result
-}
-*/
+fn main() {}
+//     println!("New worker: {:?}",
+//         OfficeWorker::from("Manuel,23,admin"));
+//     println!("New worker: {:?}",
+//         OfficeWorker::from("Jean Jacques,44,guest"));
+// }
+//
