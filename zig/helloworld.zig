a6e4bf43ff0636770385ef85c13772254bc1dc8e
Yossf-jaouhar <jwhryosf@gmail.com>
2025-09-26 22:19:44 +0100

    start in zig

--- zig/helloworld.zig
+++ zig/helloworld.zig
@@ -1,5 +1,23 @@
 const std = @import("std");
 
 pub fn main() void {
-    std.debug.print("Hello, Zig!\n", .{});
+    
+    const res: []i32 = ConvertIntegertotheSumofTwoNoZeroIntegers(9);
+    for (res) |value| {
+        std.debug.print("{}\n", .{value});
+    }
+
+}
+
+pub fn ConvertIntegertotheSumofTwoNoZeroIntegers(n: i32) []i32 {
+    var n_usize: usize = 0;
+    if (n >= 0) {
+        n_usize = @intCast(usize, n);
+    }
+
+    for (0..n_usize) |value| {
+        std.debug.print("{d}\n", .{value});
+    }
+
+    return &[_]i32{ 4, 5 };
 }
