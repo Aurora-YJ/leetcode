e06fa13de66d484162199bd5d7edf718a418b068
yojaouhar <youssefjaouhar911@gmail.com>
2025-09-30 17:28:23 +0100

    zig

--- zig/helloworld.zig
+++ zig/helloworld.zig
@@ -1,22 +1,45 @@
 const std = @import("std");
 
 pub fn main() void {
-    
-    const res: []i32 = ConvertIntegertotheSumofTwoNoZeroIntegers(9);
-    for (res) |value| {
-        std.debug.print("{}\n", .{value});
-    }
+
+    // const res: []i32 = ConvertIntegertotheSumofTwoNoZeroIntegers(69);
+    ConvertIntegertotheSumofTwoNoZeroIntegers(11);
+    // for (res) |value| {
+    //     std.debug.print("{}\n", .{value});
+    // }
 
 }
 
-pub fn ConvertIntegertotheSumofTwoNoZeroIntegers(n: i32) []i32 {
-    // var help: i32  = 1;
-   for (1..n) |v| {
-      const n1: i32 = v; 
-      const n2: i32 = n - v;
+pub fn ConvertIntegertotheSumofTwoNoZeroIntegers(n: i32) void {
+    const y: usize = @intCast(n);
+    for (1..y) |v| {
+        const n1 = v;
+        const n2 = y - n1;
 
-      if (n1 + n2 == n) {
-        std.debug.print("%d %d", .{n1 , n2});
-      }
-   }
+        const h = myitoa(n1);
+        std.debug.print("{any}\n", .{h});
+
+        if (n1 + n2 == n) {
+            
+        }
+    }
+}
+
+pub fn myitoa(n: usize) []u8 {
+    var buffer: [20]u8 = undefined;
+    var slice: []u8 = buffer[0..];
+
+    var i: usize = 0;
+
+    var num = n;
+    while (num != 0) {
+        const digit: u8 = @intCast(num % 10);
+        slice[i] = digit + '0';
+
+        num /= 10;
+        i += 1;
+    }
+    
+
+    return slice[0..i];
 }
