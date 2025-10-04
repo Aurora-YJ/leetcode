584cac3e1fb385594d6c420844b98c7d554e2fed
yojaouhar <youssefjaouhar911@gmail.com>
2025-10-04 17:10:33 +0100

    add stdin

---
+++ java/stdin.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);
+
+    System.out.println("Enter name:");
+
+    String name = myObj.nextLine();
+
+   
+
+
+    System.out.println("Name: " + name);
+  
+  }
+}
